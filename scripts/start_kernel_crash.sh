f17c7ec9e2026116d879d3f6ab33d4ab64acf753
windflex-sjlee <41511132+windflex-sjlee@users.noreply.github.com>
2018-12-12 15:11:22 +0900

    Update start_kernel_crash.sh

--- scripts/start_kernel_crash.sh
+++ scripts/start_kernel_crash.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-#yum install git -y
-#git clone https://github.com/windflex-sjlee/linux_kernel_module.git
-#linux_kernel_module/install.sh
-#linux_kernel_module/fault.sh
+# kernel-header check or install  for kernel module compile
+ll /usr/src/kernels/
+yum install kernel-devel
+ls -l /usr/src/kernels/$(uname -r)
+yum install kernel-headers
 
+# git install
 yum install git -y
 git clone https://github.com/windflex-sjlee/linux_kernel_module.git
+
+# make install and insert kernel module
 linux_kernel_module/install.sh
 
+# executing fault.sh and ask if doing them or not
 while true; do
     read -p "Do you want to make a kernel crash intentionally ? [y/n] " yn
     case $yn in
